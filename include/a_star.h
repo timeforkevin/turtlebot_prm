cb7121be658a98d5a913ae91e26456fc114b05b8
Anish Walawalkar <aswalawa@uwaterloo.ca>
2017-11-22 01:29:56 -0500

    remove edge

--- include/a_star.h
+++ include/a_star.h
@@ -4,7 +4,7 @@
 #include <set>
 
 #define SET_EDGE(M,N) (M)->adj.insert(N); (N)->adj.insert(M);
-
+#define REMOVE_EDGE(M,N) (M)->adj.erase(N); (N)->adj.erase(M);
 #define DISTANCE(X1,X2,Y1,Y2) (sqrt(((X1)-(X2))*((X1)-(X2))\
                                   + ((Y1)-(Y2))*((Y1)-(Y2))))
 #define DISTANCE_NODES(X,Y) (DISTANCE((X)->x,(Y)->x,(X)->y,(Y)->y))
